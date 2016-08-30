d0126884d9648d4e5bcb47cd40e471ebeb773a8e
Daniel Chong <danielgq.chong@gmail.com>
2016-08-31 00:12:54 +1200

    added subcomponent

--- App/Components/Counter.js
+++ App/Components/Counter.js
@@ -1,25 +1,20 @@
 import React from 'react'
 import {
   View,
-  Text,
-  TouchableOpacity,
-  StyleSheet
+  Text
 } from 'react-native'
 import Button from 'react-native-button'
+import CounterRow from './CounterRow.js'
 import styles from './Styles/Counter.Style.js'
 
 class Counter extends React.Component {
   render () {
-    let counter = this.props.store.counter
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
           Welcome to React Native Reactive!
         </Text>
-        <Text>Counter: {counter.count}</Text>
-        <Text>Total clicks: {counter.total}</Text>
-        <Button onPress={counter.increase}>+</Button>
-        <Button onPress={counter.decrease}>-</Button>
+        <CounterRow {...this.props} />
       </View>
     )
   }
